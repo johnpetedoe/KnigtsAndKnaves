09f79e060001bc01a3aa048674f9a71fdcbb2fa5
johnpetedoe <johnpetedoe@proton.me>
2026-07-14 18:35:18 -0700

    Update GRAMMAR_0.c

--- GRAMMAR_0.c
+++ GRAMMAR_0.c
@@ -1,12 +1,17 @@
 # include <stdbool.h>
 # include <stdlib.h>
 
-typedef struct    s_symbol {
-    bool    is_terminal;
-    char    *label;
+typedef struct     s_symbol {
+    bool     is_terminal;
+    char*    label;
 }                 t_symbol;
 
-typedef 
+typedef struct     s_grammar {
+    t_symbol**    non_terminal_symbols;
+    t_symbol**    terminal_symbols;
+    t_rule**      production_rules;
+}
+
 t_symbol  *new_symbol(bool is_terminal, char *label) { 
     t_symbol  *symbol;
 
@@ -16,10 +21,35 @@ t_symbol  *new_symbol(bool is_terminal, char *label) {
     return (symbol);
 }
 
+t_grammar    *new_grammar(t_symbol **terminal_symbols, t_symbol **non_terminal_symbols, t_rule **production_rules) {
+    t_grammar *grammar; 
+        
+    grammar = (t_grammar*)malloc(sizeof(t_grammar));
+
+    grammar->terminal_symbols = terminal_symbols;
+    grammar->non_terminal_symbols = non_terminal_symbols;
+    grammar->production_rules = production_rules;
+
+    return (grammar);
+}
+
+t_formula **produce_language(int breadth, int depth) {}
+    
 int main (void) {
+    t_grammar *grammar_0;
     t_symbol *ABSTRACTION;
-  
+
     ABSTRACTION = new_symbol(false, ABSTRACTION); 
     
+    grammar = new_grammar({
+        //TODO: define, instanciate, pass terminal symbols
+    }, {
+        ABSTRACTION,
+        
+    }, {
+        //TODO: define, instanciate, pass production rules
+    })
+
+    produce_language(grammar);        
     return (0);
 }
